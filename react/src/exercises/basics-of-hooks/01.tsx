5b08b3b17053e744989db567d6f7984270be57ed
Temzasse <teemu.taskula@taitounited.fi>
2019-12-31 11:12:11 +0200

    feat: add first hooks exercise

--- react/src/exercises/basics-of-hooks/01.tsx
+++ react/src/exercises/basics-of-hooks/01.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-// Basics of Hooks - Exercise 1
+/*
+Basics of Hooks - Exercise 1
+
+1. Utilize `React.useState` to store a list of animals, eg. [dog, cat, lion, elephant]
+2. Render the animals in a list
+3. Add a button to delete a specific item
+4. Add an input with a button to add a new item to the list
+
+OPTIONAL:
+- Disallow duplicate animals
+- Allow adding an animal with keyboard only (eg. submit form with Enter)
+*/
 
 const Exercise = () => {
   return (
-    <div>Basics of Hooks Exercise 1</div>
+    <div>TODO</div>
   );
 };
 
--- react/src/exercises/basics-of-hooks/final/01.tsx
+++ react/src/exercises/basics-of-hooks/final/01.tsx
@@ -3,7 +3,50 @@ import React from 'react';
 // Basics of Hooks - Exercise 1 | Final
 
 const Exercise = () => {
-  return <div>Exercise 1 final</div>;
+  const [newAnimal, setNewAnimal] = React.useState('');
+  const [animals, setAnimals] = React.useState([
+    'dog',
+    'cat',
+    'lion',
+    'elephant',
+  ]);
+
+  const removeAnimal = (animal: string) => {
+    setAnimals(prev => prev.filter(a => a !== animal));
+  };
+
+  const addAnimal = (event: any) => {
+    event.preventDefault();
+    if (!newAnimal || animals.includes(newAnimal)) return;
+    setAnimals(prev => [...prev, newAnimal]);
+    setNewAnimal('');
+  };
+
+  return (
+    <div>
+      <ul>
+        {animals.map(animal => (
+          <li key={animal}>
+            <span>{animal}</span>
+            &nbsp;&nbsp;
+            <button onClick={() => removeAnimal(animal)}>🗑</button>
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={addAnimal}>
+        <label>
+          Add an animal &nbsp;
+          <input
+            value={newAnimal}
+            onChange={({ currentTarget }) => setNewAnimal(currentTarget.value)}
+          />
+        </label>
+        &nbsp;
+        <button type="submit">➕</button>
+      </form>
+    </div>
+  );
 };
 
 // ---------------------------- DO NOT MODIFY USAGE ----------------------------
