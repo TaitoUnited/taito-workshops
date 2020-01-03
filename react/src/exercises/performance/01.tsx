5bea782de21ddde3b14ef26d6cf29ca93cd2f871
Temzasse <teemu.taskula@taitounited.fi>
2020-01-03 13:36:30 +0200

    fix: update first perf exercise

--- react/src/exercises/performance/01.tsx
+++ react/src/exercises/performance/01.tsx
@@ -3,21 +3,26 @@ import React from 'react';
 /*
 Performance - Exercise 1
 
-1. Utilize `React.useCallback` and `React.memo` to improve the rendering performance of `Inner` component
-   -> The `Inner` component should not render when the counter is incremented
+1. Utilize `React.useCallback`, `React.useMemo` and `React.memo` to improve the rendering performance of `Inner` component
+-> The `Inner` component should not render when the counter is incremented
 
-TIP: Keep in mind the callback form of the setter function provided by `React.useState` 
+TIP: Keep in mind the callback form of the setter function provided by `React.useState`
+-> https://reactjs.org/docs/hooks-reference.html#functional-updates
 */
 
-const Inner: React.FC<{ id: string; onClick: () => any }> = ({
-  id,
-  onClick,
-}) => {
+interface InnerProps {
+  id: string;
+  data: any;
+  onClick: () => any;
+}
+
+const Inner: React.FC<InnerProps> = ({ id, data, onClick }) => {
   console.log(`Inner ${id} rendered`);
 
   return (
     <div>
       <p>{Math.random()}</p>
+      <p>{JSON.stringify(data)}</p>
       <button onClick={onClick}>Click {id} to also increment</button>
     </div>
   );
@@ -26,13 +31,14 @@ const Inner: React.FC<{ id: string; onClick: () => any }> = ({
 const Exercise = () => {
   const [counter, setCounter] = React.useState(0);
   const inc = () => setCounter(counter + 1);
+  const data = { a: 1, b: 2, c: 3 };
 
   return (
     <div>
       {counter}&nbsp;
       <button onClick={inc}>➕</button>
-      <Inner id="A" onClick={inc} />
-      <Inner id="B" onClick={inc} />
+      <Inner id="A" data={data} onClick={inc} />
+      <Inner id="B" data={data} onClick={inc} />
     </div>
   );
 };
--- react/src/exercises/performance/final/01.tsx
+++ react/src/exercises/performance/final/01.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 
 // Performance - Exercise 1 | Final
 
-const Inner: React.FC<{ id: string; onClick: () => any }> = React.memo(
-  ({ id, onClick }) => {
-    console.log(`Inner ${id} rendered`);
+interface InnerProps {
+  id: string;
+  data: any;
+  onClick: () => any;
+}
 
-    return (
-      <div>
-        <p>{Math.random()}</p>
-        <button onClick={onClick}>Click {id} to also increment</button>
-      </div>
-    );
-  }
-);
+const Inner: React.FC<InnerProps> = React.memo(({ id, data, onClick }) => {
+  console.log(`Inner ${id} rendered`);
+
+  return (
+    <div>
+      <p>{Math.random()}</p>
+      <p>{JSON.stringify(data)}</p>
+      <button onClick={onClick}>Click {id} to also increment</button>
+    </div>
+  );
+});
 
 const Exercise = () => {
   const [counter, setCounter] = React.useState(0);
   const inc = React.useCallback(() => setCounter(c => c + 1), []);
+  const data = React.useMemo(() => ({ a: 1, b: 2, c: 3 }), []);
 
   return (
     <div>
       {counter}&nbsp;
       <button onClick={inc}>➕</button>
-      <Inner id="A" onClick={inc} />
-      <Inner id="B" onClick={inc} />
+      <Inner id="A" data={data} onClick={inc} />
+      <Inner id="B" data={data} onClick={inc} />
     </div>
   );
 };
